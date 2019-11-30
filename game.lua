749a33c705e4f2ae55e5626f6ba1107d5df59cf3
Marantesss <up201706917@fe.up.pt>
2019-11-30 13:36:40 +0000

    Defined keys and screen

--- game.lua
+++ game.lua
@@ -1,4 +1,40 @@
--- Screen
-screen = {
-
+-- Tic-80 Screen
+Screen = {
+	-- 240x136 display
+	width = 240, height = 136,
+	-- 60Hz refresh rate
+	refresh_rate = 60,
 }
+
+-- Keyboard functions
+Keyboard = {
+	-- W
+	w_key = function (self) return key(23) end,
+	-- A
+	a_key = function (self) return key(1) end,
+	-- S
+	s_key = function (self) return key(19) end,
+	-- D
+	d_key = function (self) return key(4) end,
+	-- UP
+	up_key = function (self) return key(58) end,
+	-- DOWN
+	down_key = function (self) return key(59) end,
+	-- LEFT
+	left_key = function (self) return key(60) end,
+	-- RIGHT
+	up_key = function (self) return key(61) end
+}
+
+function TIC()
+	cls()
+	a = Keyboard
+	b = Keyboard
+	if a:w_key() then
+		print(Screen.width,84,84)
+	end
+
+	if b:a_key() then
+		print(Screen.width,100,100)
+	end
+end
