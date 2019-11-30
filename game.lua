77722c252f90730dd1bcc0163bcc26b537d3a0cb
Marantesss <up201706917@fe.up.pt>
2019-11-30 14:46:34 +0000

    More spring cleaning

--- game.lua
+++ game.lua
@@ -3,26 +3,18 @@
 ----------------- TIC-80 SCREEN -----------------
 -------------------------------------------------
 Screen = {
-	-- 240x136 display
-	width = 240, height = 136,
-	-- 60Hz refresh rate
-	refresh_rate = 60,
+	width = 240, height = 136,		-- 240x136 display
+	refresh_rate = 60 				-- 60Hz refresh rate
 }
 -- clear screen
 function Screen:clear() cls() end
 
 -------------------------------------------------
+------------------- Singleton -------------------
 ---------------- TIC-80 KEYBOARD ----------------
 -------------------------------------------------
 -- TIC-80 Keyboard
 Keyboard = {}
--- CONSTRUCTOR --
-Keyboard.__index = Keyboard
-function Keyboard:new()
-	local kb = {}             	-- our new object
-	setmetatable(kb, Keyboard)  -- make Account handle lookup
-	return kb
-end
 -- WASD
 function Keyboard:w_key() return key(23) end
 function Keyboard:a_key() return key(1) end
@@ -33,6 +25,10 @@ function Keyboard:up_key() return key(58) end
 function Keyboard:down_key() return key(59) end
 function Keyboard:left_key() return key(60) end
 function Keyboard:right_key() return key(61) end
+
+-------------------------------------------------
+----------------- Collider Box ------------------
+-------------------------------------------------
 
 function TIC()
 	Screen.clear()
