b965b3c5e938f65e292bd398379c2fc34a039457
Marantesss <up201706917@fe.up.pt>
2019-11-30 14:20:51 +0000

    Figured out how to make objects

--- game.lua
+++ game.lua
@@ -1,4 +1,5 @@
--- Tic-80 Screen
+-- Singleton
+-- TIC-80 Screen
 Screen = {
 	-- 240x136 display
 	width = 240, height = 136,
@@ -6,35 +7,52 @@ Screen = {
 	refresh_rate = 60,
 }
 
--- Keyboard functions
+-- clear screen
+function Screen:clear() cls() end
+
+-- TIC-80 Keyboard
 Keyboard = {
-	-- W
-	w_key = function (self) return key(23) end,
-	-- A
-	a_key = function (self) return key(1) end,
-	-- S
-	s_key = function (self) return key(19) end,
-	-- D
-	d_key = function (self) return key(4) end,
-	-- UP
-	up_key = function (self) return key(58) end,
-	-- DOWN
-	down_key = function (self) return key(59) end,
-	-- LEFT
-	left_key = function (self) return key(60) end,
-	-- RIGHT
-	up_key = function (self) return key(61) end
+	-- ATTRIBUTES --
+	test = 1
 }
 
+-- CONSTRUCTOR --
+Keyboard.__index = Keyboard
+function Keyboard:new(test)
+	local kb = {}             	-- our new object
+	setmetatable(kb, Keyboard)  -- make Account handle lookup
+	kb.test = test      		-- initialize our object
+	return kb
+end
+
+-- METHODS -- 
+-- W
+function Keyboard:w_key() return key(23) end
+-- A
+function Keyboard:a_key() return key(1) end
+-- S
+function Keyboard:s_key() return key(19) end
+-- D
+function Keyboard:d_key() return key(4) end
+-- UP
+function Keyboard:up_key() return key(58) end
+-- DOWN
+function Keyboard:down_key() return key(59) end
+-- LEFT
+function Keyboard:left_key() return key(60) end
+-- RIGHT
+function Keyboard:right_key() return key(61) end
+
 function TIC()
-	cls()
-	a = Keyboard
-	b = Keyboard
+	Screen.clear()
+	a = Keyboard:new(1)
+	b = Keyboard:new(2)
 	if a:w_key() then
-		print(Screen.width,84,84)
+		print(a.test,84,84)
 	end
 
 	if b:a_key() then
-		print(Screen.width,100,100)
+		print(b.test,100,100)
 	end
 end
+
